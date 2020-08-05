0dc92b313810079ca4a5e33c84a32437492fa4b3
Knight, Eddie <iv.eddieknight@gmail.com>
2020-08-05 13:24:47 -0500

    Adjusted view/index.js with no arg error & branding additions

--- view/index.js
+++ view/index.js
@@ -1,12 +1,20 @@
 var reporter = require('cucumber-html-reporter');
 
+if (process.argv[2]) {
+        jsonDir = process.argv[2]
+} else {
+        throw TypeError('Probr HTML Generator expects exactly one argument.')
+}
+
 var options = {
         theme: 'bootstrap',                        
-        jsonDir: `${process.argv[2]}`,
-        output: `${process.argv[2]}/cucumber_report.html`,
+        jsonDir: jsonDir,
+        output: `${jsonDir}/cucumber_report.html`,
         reportSuiteAsScenarios: true,
         scenarioTimestamp: true,
         launchReport: true,
+        name: "Probr - Compliance as Code",
+        brandTitle: "Citihub Digital",
         metadata: {
             "App Version":"0.3.2",
             "Test Environment": "DEV",            
