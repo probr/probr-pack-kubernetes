b5c17ed9a64388d204e728276249deb03a692aff
Eddie Knight <iv.eddieknight@gmail.com>
2021-02-24 12:59:52 -0500

    Set probe concurrency to 2 (#359)

--- service_packs/coreengine/feature_probe_handler.go
+++ service_packs/coreengine/feature_probe_handler.go
@@ -58,10 +58,11 @@ func inMemGodogProbeHandler(gd *GodogProbe) (int, *bytes.Buffer, error) {
 func runTestSuite(o io.Writer, gd *GodogProbe) (int, error) {
 	tags := config.Vars.GetTags()
 	opts := godog.Options{
-		Format: config.Vars.ResultsFormat,
-		Output: colors.Colored(o),
-		Paths:  []string{gd.FeaturePath},
-		Tags:   tags,
+		Concurrency: 2,
+		Format:      config.Vars.ResultsFormat,
+		Output:      colors.Colored(o),
+		Paths:       []string{gd.FeaturePath},
+		Tags:        tags,
 	}
 
 	status := godog.TestSuite{
